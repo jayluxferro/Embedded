02791d110c1d97ef6c25e5f881a91a76dc8c0601
Jay Lux Ferro <jayluxferro@sperixlabs.org>
2018-03-29 18:33:49 +0000

    Initial commit

---
+++ arduino/smart_stick/smart_stick.ino
@@ -0,0 +1,106 @@
+/*
+  Project: Smart Blind Stick
+ Date: 29th March, 2018
+ */
+#include <RH_ASK.h>
+#include <SPI.h> // Not actualy used but needed to compile
+
+#define trigPin 9
+
+#define echoPin 8
+
+
+#define motor 7
+
+#define buzzer 6
+
+RH_ASK driver;
+
+void setup(){
+  pinMode(trigPin, OUTPUT);
+
+  pinMode(echoPin, INPUT);
+
+  pinMode(motor, OUTPUT);
+
+  pinMode(buzzer,OUTPUT);
+  Serial.begin(9600);
+  if (!driver.init())
+    Serial.println("init failed");
+
+}
+
+
+void loop(){
+  uint8_t buf[12];
+  uint8_t buflen = sizeof(buf);
+  if (driver.recv(buf, &buflen)) // Non-blocking
+  {
+    int i;
+    // Message with a good checksum received, dump it.
+    Serial.print("Message: ");
+    Serial.println((char*)buf);    
+    trigger();    
+  }
+  long duration, distance;
+
+  digitalWrite(trigPin, LOW); 
+
+  delayMicroseconds(2); 
+
+  digitalWrite(trigPin, HIGH);
+
+  delayMicroseconds(10); 
+
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+
+  //distance = (duration/2) / 29.1;
+  distance= duration*0.034/2;
+
+  if (distance  < 100)// This is where checking the distanceyou can change the value
+
+  { 
+     trigger();
+  } 
+  else
+
+  {
+
+    digitalWrite(motor,LOW);// when greater than 100cm
+
+    digitalWrite(buzzer,LOW); 
+
+  } 
+  delay(500);
+
+  Serial.println(distance);
+  //Serial.println(moisture());
+}
+
+long moisture(){
+  /*
+    output_value= analogRead(sensor_pin);
+   
+   output_value = map(output_value,550,0,0,100);
+   */
+  return analogRead(A0);
+}
+
+
+void trigger(){
+    digitalWrite(motor,HIGH); // When the the distance below 100cm
+
+    digitalWrite(buzzer,HIGH);
+
+    delay(500);
+
+    digitalWrite(motor, LOW);
+
+    digitalWrite(buzzer, LOW);
+
+    delay(500);
+}
+
+
---
+++ arduino/smart_stick/transmitter_stick/transmitter_stick.ino
@@ -0,0 +1,43 @@
+/*
+  Project: Smart Blink Stick
+ Date:    29th March, 2018
+ Transmitter
+ */
+
+#include <RH_ASK.h>
+#include <SPI.h> // Not actually used but needed to compile
+
+RH_ASK driver;
+
+const int buttonPin = 2;     // the number of the pushbutton pin
+int buttonState = 0; 
+
+void setup()
+{
+  pinMode(buttonPin, INPUT);
+  Serial.begin(9600);   // Debugging only
+  if (!driver.init())
+    Serial.println("init failed");
+}
+
+void loop()
+{
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // send data
+    Serial.println("Finding smart blind stick...");
+    
+    const char *msg = "Find stick!";
+    driver.send((uint8_t *)msg, strlen(msg));
+    driver.waitPacketSent();
+    delay(1000);    
+  } 
+  else {
+    // Do nothing
+  }
+
+}
+
+
